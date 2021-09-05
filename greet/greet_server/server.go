29894439f874890f147ca051c5904400f49e12c4
Prithvipal <prithvirathore99@gmail.com>
2021-09-05 13:00:41 +0530

    server streaming server side code

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/Prithvipal/go-grpc/greet/greetpb"
 	"google.golang.org/grpc"
@@ -16,6 +18,20 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 	fmt.Printf("Greet function was involked with %v \n", req)
 	result := "Hello " + req.Greeting.GetFirstName()
 	return &greetpb.GreetResponse{Result: result}, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("GreetManyTimes function was involked with %v \n", req)
+	firstName := req.Greeting.FirstName
+	for i := 0; i < 10; i++ {
+		result := "Hello " + firstName + " number " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1 * time.Second)
+	}
+	return nil
 }
 
 func main() {
