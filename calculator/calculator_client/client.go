be0618cf97f9a69ba0ad99f7272b37dffafb737e
Prithvipal <prithvirathore99@gmail.com>
2021-09-05 13:54:19 +0530

    PrimeNumberDecomposition client code

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/Prithvipal/go-grpc/calculator/calculatorpb"
@@ -19,10 +20,36 @@ func main() {
 
 	defer cc.Close()
 	c := calculatorpb.NewCalculatorServiceClient(cc)
+
+	// doSum(c)
+	doPrimeNumberDecomposition(c)
+}
+
+func doSum(c calculatorpb.CalculatorServiceClient) {
 	req := &calculatorpb.CalculateRequest{FirstNum: 10, SecondNum: 20}
 	res, err := c.Sum(context.Background(), req)
 	if err != nil {
 		log.Fatalf("Error while making sum service request %v", err)
 	}
 	fmt.Println("Result", res.Result)
+}
+
+func doPrimeNumberDecomposition(c calculatorpb.CalculatorServiceClient) {
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Num: 120,
+	}
+	reqStream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling PrimeNumberDecomposition rpc... %v", err)
+	}
+	for {
+		msg, err := reqStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading streaming: %v", err)
+		}
+		log.Println("Result: ", msg.GetResult())
+	}
 }
