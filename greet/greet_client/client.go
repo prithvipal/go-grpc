a0d50b6e1f12c1f9c11619e50b2dbd0c7187abe7
Prithvipal <prithvirathore99@gmail.com>
2021-09-05 13:24:40 +0530

    GreetManyTimes server streaming client code

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/Prithvipal/go-grpc/greet/greetpb"
@@ -19,7 +20,8 @@ func main() {
 
 	c := greetpb.NewGreetServiceClient(cc)
 	// fmt.Printf("Created client %v", c)
-	doUnary(c)
+	// doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -35,4 +37,29 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("Error while calling Greet RPC: %v", err)
 	}
 	log.Printf("Response from Greet: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to server streaming RPC")
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Prithvipal",
+			LastName:  "Singh",
+		},
+	}
+	reqStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes RPC: %v", err)
+	}
+	for {
+		msg, err := reqStream.Recv()
+		if err == io.EOF {
+			// we have reached end of stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading stream %v", err)
+		}
+		log.Println("Response from GreetManyTimes:", msg.GetResult())
+	}
 }
